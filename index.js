3736368c574d4f36c9a65439f3b07ec69bd30487
evelynKdc <evelyndc_10@hotmail.com>
2023-05-29 01:35:45 -0500

    feat: Add encrypt, decrypt and copy funcionality

---
+++ index.js
@@ -0,0 +1,48 @@
+/*-----------------declaration of variables------------*/
+const btnEncrypt = document.getElementById("encryptButton");
+const btnDecrypt = document.getElementById("decryptButton");
+const txtInput = document.getElementById("textToTransform");
+const textResult = document.getElementById("textResult");
+const copyButton = document.getElementById("copybutton");
+const messageContainer = document.getElementById("messageContainer");
+
+
+
+
+/*-----------------declaration of arrow functions------------*/
+
+const encrypt = () => {
+  const text = txtInput.value;
+  let encrypted = text.replace(/e/gim, "enter");
+  encrypted = encrypted.replace(/i/gim, "ines");
+  encrypted = encrypted.replace(/a/gim, "ai");
+  encrypted = encrypted.replace(/o/gim, "ober");
+  encrypted = encrypted.replace(/u/gim, "ufat");
+  messageContainer.classList.add("unshow");
+  copyButton.classList.add("show");
+  textResult.classList.add("show");
+  textResult.innerHTML = encrypted;
+};
+
+const descrypt = () => {
+  const textDecrypt = txtInput.value;
+  let decrypted = textDecrypt.replace(/enter/gim, "e");
+  decrypted = decrypted.replace(/ines/gim, "i");
+  decrypted = decrypted.replace(/ai/gim, "a");
+  decrypted = decrypted.replace(/ober/gim, "o");
+  decrypted = decrypted.replace(/ufat/gim, "u");
+  messageContainer.classList.add("unshow");
+  copyButton.classList.add("show");
+  textResult.classList.add("show");
+  textResult.innerHTML = decrypted;
+};
+
+const copy = () => {
+  textResult.select(); //select the content
+  document.execCommand("copy"); //copy in the clipboard the content
+};
+
+/*-----------------------EVENTS---------------------*/
+btnEncrypt.addEventListener("click", () => encrypt());
+btnDecrypt.addEventListener("click", () => descrypt());
+copyButton.addEventListener("click", () => copy());
